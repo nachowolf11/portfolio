4a28419dbdd0f6a4042dfb67124c6721d58038e5
nachowolf <ignaciowolf10@gmail.com>
2023-09-28 17:20:43 -0300

    Proyect in progress

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,13 +1,7 @@
-import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 export const MainSection = ({headerHeight}) => {
 const {t} = useTranslation("global");
-const [imageLoaded, setImageLoaded] = useState(false);
-
-const handleImageLoad = () => {
-  setImageLoaded(true);
-};
 
   return (
     <section id='main' className='section-container min-h-screen pt-28 md:pt-40'>
@@ -17,12 +11,10 @@ const handleImageLoad = () => {
           <h4 className='subtitle'>{t("mainSection.subtitle")}</h4>
         </div>
 
-        <div className='profilePicture-container animate__animated animate__fadeIn'>
+        <div className='profilePicture-container'>
           <img 
-            className={`animate__animated ${imageLoaded ? 'animate__fadeIn' : ''}`}
             src="./img/cv.PNG"
             alt="Ignacio Wolf"
-            onLoad={handleImageLoad}
           />
         </div>    
     </section>
