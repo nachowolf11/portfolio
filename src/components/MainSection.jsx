ec84b985e7d28d625fdf43387772660abe0558dc
nachowolf <ignaciowolf10@gmail.com>
2023-10-04 20:21:37 -0300

    update

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,15 +1,22 @@
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 export const MainSection = () => {
 const {t} = useTranslation("global");
 
   return (
+    <>
+
+    <Typist>
+    <h1>Hola</h1>
+  </Typist>
     <section className='section-container min-h-screen pt-28 md:pt-40'>
 
         <div className='title-container'>
           <h2 className='title'>{t("mainSection.title")}</h2>
           <h4 className='subtitle'>{t("mainSection.subtitle")}</h4>
         </div>
+
 
         <div className='profilePicture-container'>
           <img 
@@ -18,5 +25,6 @@ const {t} = useTranslation("global");
           />
         </div>    
     </section>
+    </>
   )
 }
