f3966b22b0d1a24b3ac2237a4996b199c061f960
nachowolf <ignaciowolf10@gmail.com>
2023-10-09 10:00:41 -0300

    lazy loading

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export const MainSection = () => {
 const {t} = useTranslation("global");
+
+useEffect(() => {
+  const img = document.getElementById('cv-img');
+
+  const loaded = () => {
+    img.classList.add('img-loaded');
+  }
+
+  if(img.complete){
+    loaded();
+  }else{
+    img.addEventListener('load', loaded);
+  }
+}, [])
+
 
   return (
     <section className='section-container min-h-screen pt-28 md:pt-40'>
@@ -13,10 +29,12 @@ const {t} = useTranslation("global");
 
 
         <div className='profilePicture-container'>
-          <img 
+          <img
+            id='cv-img'
             src="./img/cv2.png"
             alt="Ignacio Wolf"
             loading='lazy'
+            className='img-not-loaded'
           />
         </div>    
     </section>
