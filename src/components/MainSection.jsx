80ac52e19fe8f2ca09697450d737a490160042e1
nachowolf <ignaciowolf10@gmail.com>
2023-10-10 15:35:00 -0300

    arrowdown

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import * as Scroll from 'react-scroll';
+
 
 export const MainSection = () => {
+let Link = Scroll.Link;
 const {t} = useTranslation("global");
 
 useEffect(() => {
@@ -36,7 +39,18 @@ useEffect(() => {
             loading='lazy'
             className='img-not-loaded'
           />
-        </div>    
+        </div>
+        <Link activeClass="activeSection" className='text-center font-kanit text-xl' to="aboutme" spy={true} smooth={true} offset={0} duration={600}>
+          <svg className='hover:cursor-pointer arrowdown' xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+            <polygon points="10,30 50,70 90,30" fill="rgba(0, 0, 0, 0.77)" stroke="" stroke-width="5" />
+            <polygon points="10,40 50,80 90,40" fill="rgba(0, 0, 0, 0.521)" stroke="" stroke-width="5" opacity="0.6" filter="url(#blur)" />
+            <defs>
+              <filter id="blur">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="3" />
+              </filter>
+            </defs>
+          </svg>
+        </Link>
     </section>
   )
 }
