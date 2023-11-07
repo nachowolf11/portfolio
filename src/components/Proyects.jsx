72d3b9d26087a7a655e52f0e931a396b2b1bd8e8
nachowolf <ignaciowolf10@gmail.com>
2023-11-07 20:22:10 -0300

    Nuevo diseño de seccion proyects terminado

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -7,12 +7,8 @@ export function Proyects() {
     const { t } = useLanguage('global');
     const [expandedIndex, setExpandedIndex] = useState(null)
 
-    const handleMouseOver = (index) => {
-        setExpandedIndex(index)
-    }
-
-    const handleMouseOut = (index) => {
-        setExpandedIndex(-1)
+    const handleClick = (id) => {
+        setExpandedIndex(id === expandedIndex ? -1 : id)
     }
 
     const cardVariants = {
@@ -22,7 +18,7 @@ export function Proyects() {
         },
         collapsed: {
             width: window.innerWidth > 880 ? "350px" : "260px",
-            opacity: 0.5
+            opacity: 0.8
         }
     }
 
@@ -31,9 +27,9 @@ export function Proyects() {
             <div className="flex justify-center mb-16">
                 <div>
                     <div className="flex flex-col items-center h-full gap-5 px-2 md:flex-row">
-                        {ProyectsData.map(({ id, title, imageUrl, description, skills }) => (
+                        {ProyectsData.map(({ id, title, coverImage, appImage, description, skills, url }) => (
                             <motion.div key={id}
-                                className={`cursor-pointer h-[400px] bg-auto bg-no-repeat bg-center rounded-[20px] mx-2 ${id === expandedIndex && 'expanded'}`}
+                                className={`cursor-pointer h-[400px] bg-cover bg-no-repeat bg-center rounded-[20px] mx-2 shadow-md shadow-slate-400 ${id === expandedIndex && 'expanded'}`}
                                 initial={{ opacity: 1 }}
                                 variants={cardVariants}
                                 animate={id === expandedIndex ? 'expanded' : 'collapsed'}
@@ -41,18 +37,27 @@ export function Proyects() {
                                     opacity: 1
                                 }}
                                 transition={{ duration: 0.5 }}
-                                onMouseOver={() => handleMouseOver(id)}
-                                onMouseOut={() => handleMouseOut(id)}
+                                onClick={()=>{handleClick(id)}}
                                 style={{
-                                    backgroundImage: `url(${imageUrl})`
+                                    backgroundImage: `url(${id === expandedIndex ? appImage : coverImage})`
                                 }}
                             >
-                                <div className="flex flex-col justify-end h-full">
+                                <div className={`flex flex-col ${id === expandedIndex && url !== '' ? 'justify-between' : 'justify-end'} h-full`}>
+                                    {id === expandedIndex && url !== '' && (
+                                        <>
+                                        <div className=' flex justify-center mt-32'>
+                                            <a href={url} target='_blank'
+                                                className='text-white font-normal select-none text-xl bg-gray-800 bg-opacity-75 font-kanit rounded-full px-5 py-3 hover:shadow-lg hover:shadow-white transition-all'>
+                                                {t('projects.visitWeb')}
+                                            </a>
+                                        </div>
+                                        </>
+                                    )}
                                     <div className="card-footer rounded-b-[20px] bg-gray-800 bg-opacity-75 min-h-[100px] flex flex-col items-center justify-center p-3">
-                                        <h2 className="font-kanit text-xl font-semibold text-center text-white">{title}</h2>
+                                        <h2 className="font-kanit text-xl font-semibold text-center text-white select-none">{title}</h2>
                                         {id === expandedIndex && (
                                             <>
-                                                <p className='text-white font-kanit font-extralight text-lg'>{t(description)}</p>
+                                                <p className='text-white font-kanit font-extralight text-lg select-none'>{t(description)}</p>
                                                 <div className="flex gap-5 mt-3">
                                                     {skills.map((data, index) => (
                                                         <p key={index}>{data.icon}</p>
--- src/utils/ProyectsData.jsx
+++ src/utils/ProyectsData.jsx
@@ -4,9 +4,10 @@ export const ProyectsData = [
   {
     id: 0,
     title: "Twitter Clone",
-    description:
-      "projects.twitter",
-    imageUrl: "./img/fondotwitter.png",
+    description: "projects.twitter",
+    url:"https://twitterclone-av6b-dev.fl0.io/",
+    coverImage: "./img/fondotwitter.png",
+    appImage: "./img/twitterweb.png",
     skills: [
       {
         icon: <SiReact size="40" />
@@ -22,9 +23,10 @@ export const ProyectsData = [
   {
     id: 1,
     title: "Finance App",
-    description:
-      "projects.financeapp",
-    imageUrl: "./img/fondoPokeapp.png",
+    description: "projects.financeapp",
+    url:"",
+    coverImage: "./img/fondoFinanceapp.png",
+    appImage: "./img/financeappweb.gif",
     skills: [
         {
             icon: <SiReact size="40" />
